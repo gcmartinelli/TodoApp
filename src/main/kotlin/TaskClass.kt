ccb4032a1b38041fa0a38a0b17b37f844668d3a6
gcmartinelli <gcmartinelli@gmail.com>
2019-09-06 18:16:46 +0000

    Adding some documentation

--- src/main/kotlin/TaskClass.kt
+++ src/main/kotlin/TaskClass.kt
@@ -1,3 +1,17 @@
+/*  A data class representing a task in the ToDo app.
+
+    The class is instantiated with the following parameters:
+        * title : String -> A short title for the task
+        * description : String -> A short description
+        * dueDate : Date -> When the task is due
+
+    Custom methods include:
+        * updateStatus (Returns a boolean representing if
+                        the task is past its due date)
+        * complete (Marks the task status as complete)
+        * delete (Deletes the task)
+ */
+
 import java.util.Date
 
 data class Task(val creationDate: Date = Date(),
