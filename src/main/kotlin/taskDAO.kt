1c90666605a1a7b3f369756e065afee31c865b6f
gcmartinelli <gcmartinelli@gmail.com>
2019-09-06 21:39:32 +0000

    Method to add task implemented
    Also changed the current data store to a mutable list just to make things simpler. Removed the getTask method for the time being since I can just return the tasks list when needed. I'm sure in the future I'll have a dedicated method since I'll need to interact with a database and probably do some filtering as needed

--- src/main/kotlin/taskDAO.kt
+++ src/main/kotlin/taskDAO.kt
@@ -7,21 +7,30 @@ import java.util.Date
 class TaskDAO(){
     // I'll initially work with data stored in memory.
     // For convenience I'll initialize some here
-    val tasks = hashMapOf(
-        0 to Task(0, Date(), Date(2019-1900, 9, 21), title="A simple task", description="Things to do", status="todo"),
-        1 to Task(1, Date(), Date(2019-1900, 8, 9), title="Buy eggs", description="Can't make omeletes", status="todo"),
-        2 to Task(2, Date(), Date(2019-1900, 8, 5), title="Learn Kotlin", description="Cool jobs require it ;)", status="late"),
-        3 to Task(3, Date(), Date(2019-1900, 10, 21), title="Buy bitcoin", description="Buy the dip and HODL", status="todo"),
-        4 to Task(4, Date(), Date(2019-1900, 8, 8), title="Water plants", description="", status="todo"),
-        5 to Task(5, Date(), Date(2019-1900, 11, 20), title="Get Xmas gifts", description="Don't leave it to the last minute", status="todo")
+    var tasks = mutableListOf(
+        Task(0, Date(), Date(2019-1900, 9, 21), title="A simple task", description="Things to do", status="todo"),
+        Task(1, Date(), Date(2019-1900, 8, 9), title="Buy eggs", description="Can't make omeletes", status="todo"),
+        Task(2, Date(), Date(2019-1900, 8, 5), title="Learn Kotlin", description="Cool jobs require it ;)", status="late"),
+        Task(3, Date(), Date(2019-1900, 10, 21), title="Buy bitcoin", description="Buy the dip and HODL", status="todo"),
+        Task(4, Date(), Date(2019-1900, 8, 8), title="Water plants", description="", status="todo"),
+        Task(5, Date(), Date(2019-1900, 11, 20), title="Get Xmas gifts", description="Don't leave it to the last minute", status="todo")
     )
 
-    fun getTasks() : List<Task> {
-        TODO()
-    }
+    fun addTask(dueDate : Date,
+                title : String,
+                desc : String = "") {
+        /* Adds a new task to the database if the due date is valid,
+            else returns an error */
+        // Check if dueDate is valid (i.e. not in the past)
+        if (Date().after(dueDate)) {
+            val message : String = "[ERROR] We can't go back in time :( This due date is already gone"
+            println(message) // TODO: Implement logging
+        } else {
+            val nextID : Int = tasks.size
+            val newTask = Task(id = nextID, dueDate = dueDate, title = title, description = desc)
+            tasks.add(newTask)
+        }
 
-    fun addTask() {
-        TODO()
     }
 
     fun delTask() {
