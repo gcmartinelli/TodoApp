ff7024da98c198b262af78f04e1d8a9689a3a4e2
gcmartinelli <gcmartinelli@gmail.com>
2019-09-06 08:32:33 +0000

    Initial commit
    Trying out Kotlin for the first time

---
+++ src/main/kotlin/TaskClass.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+class Task(creationDate: Date,
+           dueDate: Date,
+           title: String,
+           description: String,
+           status: String)
+
+    fun complete(){
+        TODO ()
+    }
+
+    fun delete(){
+        TODO ()
+    }
