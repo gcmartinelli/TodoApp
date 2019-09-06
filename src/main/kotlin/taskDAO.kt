e5ea778c61c644c3138ffb2e4ca51ca34f93348e
gcmartinelli <gcmartinelli@gmail.com>
2019-09-06 20:47:24 +0000

    bug fix: Java function does not accep named arguments

--- src/main/kotlin/taskDAO.kt
+++ src/main/kotlin/taskDAO.kt
@@ -8,12 +8,12 @@ class TaskDAO(){
     // I'll initially work with data stored in memory.
     // For convenience I'll initialize some here
     val tasks = hashMapOf(
-        0 to Task(id=0, Date(), Date(2019-1900, 9, 21), title="A simple task", description="Things to do", status="todo"),
-        1 to Task(id=1, Date(), Date(2019-1900, 8, 9), title="Buy eggs", description="Can't make omeletes", status="todo"),
-        2 to Task(id=2, Date(), Date(2019-1900, 8, 5), title="Learn Kotlin", description="Cool jobs require it ;)", status="late"),
-        3 to Task(id=3, Date(), Date(2019-1900, 10, 21), title="Buy bitcoin", description="Buy the dip and HODL", status="todo"),
-        4 to Task(id=4, Date(), Date(2019-1900, 8, 8), title="Water plants", description="", status="todo"),
-        5 to Task(id=5, Date(), Date(2019-1900, 11, 20), title="Get Xmas gifts", description="Don't leave it to the last minute", status="todo"),
+        0 to Task(0, Date(), Date(2019-1900, 9, 21), title="A simple task", description="Things to do", status="todo"),
+        1 to Task(1, Date(), Date(2019-1900, 8, 9), title="Buy eggs", description="Can't make omeletes", status="todo"),
+        2 to Task(2, Date(), Date(2019-1900, 8, 5), title="Learn Kotlin", description="Cool jobs require it ;)", status="late"),
+        3 to Task(3, Date(), Date(2019-1900, 10, 21), title="Buy bitcoin", description="Buy the dip and HODL", status="todo"),
+        4 to Task(4, Date(), Date(2019-1900, 8, 8), title="Water plants", description="", status="todo"),
+        5 to Task(5, Date(), Date(2019-1900, 11, 20), title="Get Xmas gifts", description="Don't leave it to the last minute", status="todo")
     )
 
     fun getTasks() : List<Task> {
