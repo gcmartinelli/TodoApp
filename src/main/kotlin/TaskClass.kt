11414e66698e4f4bc4d8d09ac53f1a34f1f7af2c
gcmartinelli <gcmartinelli@gmail.com>
2019-09-06 18:08:27 +0000

    New class methods
    Added an updateStatus method that returns a Boolean answering if the task is past it's due date. Also included some comments to better document the class. Made the java import more explicit.

--- src/main/kotlin/TaskClass.kt
+++ src/main/kotlin/TaskClass.kt
@@ -1,15 +1,25 @@
-import java.util.*
+import java.util.Date
 
-class Task(creationDate: Date,
-           dueDate: Date,
-           title: String,
-           description: String,
-           status: String)
+data class Task(val creationDate: Date = Date(),
+           var dueDate: Date,
+           val title: String,
+           val description: String = "",
+           var status: String) {
+
+    fun updateStatus() : Boolean {
+        /* Returns a boolean representing
+            if the task is past it's due date */
+        return Date().after(dueDate)
+    }
 
     fun complete(){
+        /* Marks the task status as complete */
         TODO ()
     }
 
     fun delete(){
+        /* Deletes the task */
         TODO ()
     }
+
+}
