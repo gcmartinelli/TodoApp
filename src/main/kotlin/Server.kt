7a2891792295bc4d260aad47faacc8d088f2ed9b
gcmartinelli <gcmartinelli@gmail.com>
2019-09-09 21:50:44 +0000

    Adding templates and interactions
    Added Mustache as templating engine. Incorporated some view handlings in the API file to handle date formating (far from ideal). Added 'delete' and 'complete' commands to each element in the task list

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,8 +1,8 @@
 /* Runs the To-do App API server */
 
-import TaskDAO
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder
+import java.util.Date
 
 fun main() {
     val taskDAO = TaskDAO()
@@ -15,11 +15,31 @@ fun main() {
     app.routes {
         /* where the magic happens */
         ApiBuilder.get("/tasks") { ctx ->
-            val tasks = taskDAO.tasks.filter{ it.value.deleted == false } // Let's ignore deleted tasks ofc
-            ctx.json(tasks)
+            val tasks = taskDAO.tasks.filter{ !it.value.deleted }.values // Let's ignore deleted tasks ofc
+            /* The block below transforms the date fields from unix time to strings.
+                There probably is a more concise way of doing this. And I could be doing this in
+                a dedicated 'views' file instead of mixing this logic with the API logic. But given the
+                small scope of the project I'll keep this as is for now.
+             */
+            class RenderTaskClass(val id:Int, val creationDate:String, val dueDate:String,
+                             val title:String, val desc:String, val status:String){} // Temp class
+            val tasksToRender = mutableListOf<RenderTaskClass>() // Instantiate an empty list
+            for (task in tasks) { // Iterate over the filtered tasks, transforming the date values
+                tasksToRender.add(
+                    RenderTaskClass(
+                        id = task.id,
+                        creationDate = Date(task.creationDate).toString(),
+                        dueDate = Date(task.dueDate).toString(),
+                        title = task.title, desc = task.description,
+                        status = task.status
+                    )
+                )
+            }
+           ctx.render("tasks.mustache",  mapOf("tasks" to tasksToRender))
         }
 
         ApiBuilder.post("/tasks/add") { ctx ->
+            // A lot of repetition in this block... need to refactor
             val dueDate = ctx.formParam("dueDate")?.toLong() ?: throw IllegalArgumentException("[ERROR] DueDate cannot be Null")
             val title = ctx.formParam("title") ?: throw IllegalArgumentException("[ERROR] Title cannot be Null")
             val desc = ctx.formParam("desc")
@@ -29,22 +49,24 @@ fun main() {
                 } else {
                     taskDAO.addTask(dueDate, title)}
                 ctx.status(200)
+                ctx.redirect("/tasks")
             } catch (e: IllegalArgumentException) {
                 ctx.status(500)
             }
         }
 
-        ApiBuilder.delete("/tasks/delete/:taskID") { ctx ->
+        ApiBuilder.post("/tasks/delete/:taskID") { ctx ->
             val taskID = ctx.pathParam("taskID").toInt()
             taskDAO.delTask(taskID).also{ println("[DELETED] Task with ID $taskID set to deleted.")}
-
             ctx.status(200)
+            ctx.redirect("/tasks")
         }
 
-        ApiBuilder.patch("/tasks/complete/:taskID") { ctx ->
+        ApiBuilder.post("/tasks/complete/:taskID") { ctx ->
             val taskID = ctx.pathParam("taskID").toInt()
             taskDAO.completeTask(taskID).also{ println("[UPDATED] Task with ID $taskID status set to complete.")}
             ctx.status(200)
+            ctx.redirect("/tasks")
         }
     }
 
